4dc122f27e0c2dc94691f76f3ade0716c5650296
Sherali_1116 <sheralitukhtayev@gmail.com>
2022-02-28 22:30:51 +0500

    changes

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,14 @@
-// alert('hello');
-console.log('hello!');
-// prompt('What is your age?');
-
-function Age(){
-  console.log('It is working');
-  ageResult = prompt('What is your Year?');
-  console.log('Your age in days in ' + ((2022- ageResult) * 365));
-  document.getElementById('Age').innerHTML = 'Your age in days ' + ((2022 - ageResult) * 365);
+//Challenge 1: Age in days
+function Age() {
+  // console.log('It is working');
+  var ageResult = prompt('What year were you born... Goood friend?');
+  // console.log('Your age in days in ' + ((2022- ageResult) * 365));
+  document.getElementById('flex-box-result').innerHTML = 'Your age in days ' + ((2022 - ageResult) * 365);
 }
 Age();
 
 function Remove() {
-  console.log('it is working');
-  document.getElementById('Age').innerHTML = ''
+  // console.log('it is working');
+  document.getElementById('flex-box-result').innerHTML = ''
 }
 Remove();
