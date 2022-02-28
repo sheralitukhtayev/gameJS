d9253ff1c531127c1aca05b8a3d6cf0af613461f
Sherali_1116 <sheralitukhtayev@gmail.com>
2022-02-28 19:12:14 +0500

    💯

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,17 @@
 // alert('hello');
 console.log('hello!');
+// prompt('What is your age?');
+
+function Age(){
+  console.log('It is working');
+  ageResult = prompt('What is your Year?');
+  console.log('Your age in days in ' + ((2022- ageResult) * 365));
+  document.getElementById('Age').innerHTML = 'Your age in days ' + ((2022 - ageResult) * 365);
+}
+Age();
+
+function Remove() {
+  console.log('it is working');
+  document.getElementById('Age').innerHTML = ''
+}
+Remove();
