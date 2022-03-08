f233cb678c76f3fc7042bbf84af11e30fb6403ca
Sherali_1116 <sheralitukhtayev@gmail.com>
2022-03-08 08:26:38 +0500

    changes

--- static/js/index.js
+++ static/js/index.js
@@ -151,7 +151,7 @@ let blackjackGame = {
   'you': {'scoreSpan': '#your-result', 'div': '#your-box', 'score': 0},
   'dealer': {'scoreSpan': '#dealer-result', 'div': '#dealer-box', 'score': 0},
   'cards': ['2' ,'3', '4', '5', '6', '7', '8', '9', '10' , 'Jack' , 'Queen', 'King', 'Poker'],
-  'cardsMap': {'2':2 ,'3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10 , 'Jack':10 , 'Queen':10, 'King':10, 'Poker': [1]},
+  'cardsMap': {'2':2 ,'3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10 , 'Jack':10 , 'Queen':10, 'King':10, 'Poker': 1},
 };
 
 const YOU = blackjackGame['you']
